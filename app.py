ba4c762be15c237dc34f9e5b87436facab7ea1d7
SnehaDadhich Github <snehadadhich622@gmail.com>
2026-01-30 12:11:44 +0530

    Rename 1.py to app.py for gunicorn

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
+from flask import Flask
+app = Flask(__name__)
 # Load dataset
 @st.cache_data
 def load_data():
